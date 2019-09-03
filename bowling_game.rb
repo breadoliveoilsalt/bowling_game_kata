b0a3042768b8695a3fa01e38676973b37033a18a
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-03 12:53:48 -0400

    Catch error in first test for 0; pass test 4

--- bowling_game.rb
+++ bowling_game.rb
@@ -2,15 +2,15 @@ class BowlingGame
 
 	def get_score(frames)
 	
-		if frames == []
-			0
-		else 
-			throw1 = frames[0][0].to_i
-			throw2 = frames[0][1].to_i
-			score = throw1 + throw2
-			score.to_s
+		score = 0 
+	
+		frames.each do |frame|
+			score += frame[0].to_i
+			score += frame[1].to_i
 		end
-
+		
+		score.to_s
+		
 	end
 
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe BowlingGame do
 		
 		it "returns 0 when there are no frames bowled" do 
 			frames = []
-			expect(@game.get_score(frames)).to eq(0)
+			expect(@game.get_score(frames)).to eq("0")
 		end
 
 		it "adds up the score of one frame without a strike or spare" do 
