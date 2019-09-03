4a657e562d0b1adc175959d3ad72a0889429844f
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-03 12:42:03 -0400

    Refactor #score to #get_score and pass test 3

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,7 +1,16 @@
 class BowlingGame
 
-	def score(frames)
-		0
+	def get_score(frames)
+	
+		if frames == []
+			0
+		else 
+			throw1 = frames[0][0].to_i
+			throw2 = frames[0][1].to_i
+			score = throw1 + throw2
+			score.to_s
+		end
+
 	end
 
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe BowlingGame do
 		
 		it "returns 0 when there are no frames bowled" do 
 			frames = []
-			expect(@game.score(frames)).to eq(0)
+			expect(@game.get_score(frames)).to eq(0)
 		end
 
 		it "adds up the score of one frame" do 
 			frames=[
 				["1", "2"]
 			]
-			expect(@game.score(frames)).to eq("3")
+			expect(@game.get_score(frames)).to eq("3")
 		end
 	end
 end 
