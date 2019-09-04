87d1dc459f23a9184f72956802d3bba160d2ae01
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-09-04 12:22:02 -0400

    Recommit last commit for real

--- bowling_game.rb
+++ bowling_game.rb
@@ -5,12 +5,12 @@ class BowlingGame
 		score = 0 
 	
 		(0...frames.length).each do |frame|
-			pins_knocked_down_on_throw = frames[frame]
-			if pins_knocked_down_on_throw[1] == "/"
+			pins_knocked_down_on_throws = frames[frame]
+			if pins_knocked_down_on_throws[1] == "/"
 				next_frame_first_throw = frames[frame+1][0]
-				score += 10 + next_frame_first_throw.to_i
+				score += 10 + next_frame_first_throw.to_i	
 			else
-				pins_knocked_down_on_throw.each do |pins|
+				pins_knocked_down_on_throws.each do |pins|
 					score += pins.to_i
 				end
 			end
